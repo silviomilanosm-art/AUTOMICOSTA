edd0fd7e210dbabad8e193763986c3a7b3924d3c
silviomilanosm-art <silviomilanosm@gmail.com>
2026-09-07 01:36:39 +0200

    Bump AUTOMICOSTA version for update install

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.automicosta.app"
         minSdk = 23
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.3.0"
     }
 
     buildFeatures { compose = true }
