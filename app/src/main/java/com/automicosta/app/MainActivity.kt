72239c42793549adde3818ec1fde3c94ade36728
silviomilanosm-art <silviomilanosm@gmail.com>
2026-09-07 02:02:28 +0200

    Open AUTOMICOSTA through new root with parts section

--- app/src/main/java/com/automicosta/app/MainActivity.kt
+++ app/src/main/java/com/automicosta/app/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.automicosta.app.ui.AutoMiCostaApp
+import com.automicosta.app.ui.AutoMiCostaRoot
 import com.automicosta.app.ui.AutoMiCostaViewModel
 import com.automicosta.app.ui.theme.AutoMiCostaTheme
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AutoMiCostaTheme {
                 val vm: AutoMiCostaViewModel = viewModel(factory = AutoMiCostaViewModel.factory(app.database.dao()))
-                AutoMiCostaApp(vm)
+                AutoMiCostaRoot(vm)
             }
         }
     }
