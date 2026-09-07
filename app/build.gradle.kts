4500928e6f713c8a5598147b8c61742b5c2c0193
silviomilanosm-art <silviomilanosm@gmail.com>
2026-09-07 13:14:31 +0200

    Bump AUTOMICOSTA to 0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.automicosta.app"
         minSdk = 23
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.7.0"
+        versionCode = 8
+        versionName = "0.7.1"
     }
 
     buildFeatures { compose = true }
